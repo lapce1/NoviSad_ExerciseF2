95ae38221c5b038a3f33c3fd28b6d0e8192c668d
somename <someemail>
2017-06-08 20:29:45 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -8,7 +8,6 @@ public class TestPlanetExplorer {
 	@Test
 	public void test_executeCommand() throws PlanetExplorerException {
 		PlanetExplorer pe = new PlanetExplorer(3,3,"");
-		pe.executeCommand("f");
 		
 		assertEquals("Lokacija je (1,0), N","(1, 0, N)",pe.executeCommand("f"));
 	}
