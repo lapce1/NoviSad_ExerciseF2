6676053da62d733fa9ffe66931afe09561e4c3ff
somename <someemail>
2017-06-08 19:51:29 +0200

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -1,11 +1,12 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestPlanetExplorer {
 
 	@Test
 	public void test_executeCommand() {
+		PlanetExplorer(3,3,"");
+		executeCommand("f");
 		fail("Not yet implemented");
 	}
 }
